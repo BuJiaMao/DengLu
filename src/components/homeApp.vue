99b329a024b5bfb0c3d46c07445bd0addee33b12
卜嘉兴 <1297363727@qq.com>
2024-03-06 08:31:41 +0800

    完成了登录功能

---
+++ src/components/homeApp.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    Home组件！
+    <el-row>
+      <el-button type="danger" round @click="tuiChu">退出</el-button>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    tuiChu() {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    },
+  },
+}
+</script>
+
+<style lang="less" scoped>
+</style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,13 @@
 import Vue from 'vue'
 import { Button } from 'element-ui'
+import { Form, FormItem } from 'element-ui'
+import { Input } from 'element-ui'
+import { Link } from 'element-ui'
+import { Message } from 'element-ui'
 
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
 Vue.use(Button)
+Vue.use(Link)
+Vue.prototype.$Message = Message
